9e0861b22645a19dc0af8d48314032bedcc234ee
Daria Korniienko <106033233+Dashasoham@users.noreply.github.com>
2023-01-17 22:02:52 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+// 'use strict';
+
+// function calcAge(birthYear) {
+//   const age = 2031 - birthYear;
+
+//   function printAge() {
+//     let output = `${firstName} You´re ${age}, born in ${birthYear}`;
+//     console.log(output);
+//     if (birthYear >= 1981 && birthYear <= 1996) {
+//       var millenial = true;
+//       //Creating NEW variable with the same name as outer scope´s variable
+//       const firstName = 'Steven';
+//       //Reassigning outer score´s variable
+//       const output = 'NEW OUTPUT';
+
+//       const str = `Oh, you´re a millenial, ${firstName}`;
+//       console.log(str);
+
+//       function add(a, b) {
+//         return a + b;
+//       }
+//     }
+//     // console.log(str);
+//     console.log(millenial);
+//     // console.log(add(2, 3));
+//     console.log(output);
+//   }
+//   printAge();
+//   return age;
+// }
+// const firstName = 'Jonas';
+// calcAge(1991);
+// // console.log(age);
